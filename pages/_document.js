1557de02e53cc7998aeb58529332b1ff5e7b14df
admin <dinhhoainam94@gmail.com>
2018-09-12 15:54:22 +0700

    test seo

--- pages/_document.js
+++ pages/_document.js
@@ -9,22 +9,22 @@ export default class MyDocument extends Document {
       <html lang="vi">
         <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
-          {/*<noscript id="deferred-styles">*/}
-            {/*<link rel="stylesheet" type="text/css" href="/_next/static/style.css"/>*/}
-            {/*<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>*/}
-        {/*</noscript>*/}
+        <noscript id="deferred-styles">
+            <link rel="stylesheet" type="text/css" href="/_next/static/style.css"/>
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        </noscript>
 
-        {/*<script dangerouslySetInnerHTML={{__html: `var loadDeferredStyles = function() {*/}
-            {/*var addStylesNode = document.getElementById("deferred-styles");*/}
-            {/*var replacement = document.createElement("div");*/}
-            {/*replacement.innerHTML = addStylesNode.textContent;*/}
-            {/*document.body.appendChild(replacement)*/}
-            {/*addStylesNode.parentElement.removeChild(addStylesNode);*/}
-        {/*};*/}
-        {/*var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||*/}
-            {/*window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;*/}
-        {/*if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });*/}
-        {/*else window.addEventListener('load', loadDeferredStyles);`}} />*/}
+        <script dangerouslySetInnerHTML={{__html: `var loadDeferredStyles = function() {
+            var addStylesNode = document.getElementById("deferred-styles");
+            var replacement = document.createElement("div");
+            replacement.innerHTML = addStylesNode.textContent;
+            document.body.appendChild(replacement)
+            addStylesNode.parentElement.removeChild(addStylesNode);
+        };
+        var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+        else window.addEventListener('load', loadDeferredStyles);`}} />
 
         </Head>
         <body>
@@ -40,14 +40,14 @@ export default class MyDocument extends Document {
           {/*<NextScript />*/}
         </body>
         <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120211455-1" />
-        <script dangerouslySetInnerHTML={{__html : `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-120211455-1');
-        ` }}>
-        </script>
+        {/*<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120211455-1" />*/}
+        {/*<script dangerouslySetInnerHTML={{__html : `*/}
+            {/*window.dataLayer = window.dataLayer || [];*/}
+            {/*function gtag(){dataLayer.push(arguments);}*/}
+            {/*gtag('js', new Date());*/}
+            {/*gtag('config', 'UA-120211455-1');*/}
+        {/*` }}>*/}
+        {/*</script>*/}
       </html>
     )
   }
